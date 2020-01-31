e2fbc79389ea32727933a2dc68b10b845e224d02
Anri Liy <anri@realquantum.com>
2020-01-31 12:49:09 -0800

    ToDo List

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Timer from './Timer';
+import ToDoApp from './Todo';
 
 function App() {
   return (
     <div className="App">
-      <Timer />
+      <ToDoApp />
     </div>
   );
 }
