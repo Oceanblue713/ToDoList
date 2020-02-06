4e9f912708f46ce799a5eae88f4d23cf991cff15
Anri Liy <anri@realquantum.com>
2020-02-05 18:11:06 -0800

    Input Output

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MarkdownEditor from './MarkdownEditor';
 import Timer from './Timer';
 import ToDoApp from './Todo';
 
 function App() {
   return (
     <div className="App">
-      <ToDoApp />
+      <MarkdownEditor />
     </div>
   );
 }
